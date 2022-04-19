7bb5f3a5e803483c57b368f0db6b7343153a7e90
sawyermade <sawyermade0@gmail.com>
2022-04-19 14:09:05 -0400

    first

---
+++ README.md
@@ -0,0 +1,17 @@
+# Assignment 8 Python IT USF Spring 2022
+My solution to Ass8
+
+## Clone and Enter Directory:
+```
+git clone https://github.com/sawyermade/Ass8
+cd Ass8
+```
+
+## Run:
+```
+# Run Part 1
+python3 ass8.py
+
+# Run Part 2
+python3 ass8.py
+```
---
+++ roshambo.py
@@ -0,0 +1,142 @@
+import random
+
+# Player parent class
+class Player:
+	def __init__(self, name):
+		self.name = name 
+		self.w    = 0
+		self.l    = 0
+		self.val  = ''
+
+	def __str__(self):
+		return f'{self.name}: {self.val}'
+
+# Bart, Player child class
+class Bart(Player):
+	def __init__(self):
+		Player.__init__(self, 'Bart')
+
+	def generateRoshambo(self):
+		self.val = 'r'
+
+# Lisa, Player child class
+class Lisa(Player):
+	def __init__(self):
+		Player.__init__(self, 'Lisa')
+
+	def generateRoshambo(self):
+		self.val = random.choice(['r', 'p', 's'])
+
+# Play function and summary logic
+def play(player, opponent, game_count):
+	# Win/Draw flags
+	win_flag, draw_flag = True, False
+
+	# Check for draw
+	if player.val == opponent.val:
+		draw_flag = True
+
+	# If player has rock and opponent not scissors
+	elif player.val == 'r':
+		if opponent.val != 's':
+			win_flag = False
+
+	# If player has paper and opponent not rock
+	elif player.val == 'p':
+		if opponent.val != 'r':
+			win_flag = False
+
+	# If player has scissors and opponent not paper
+	else:
+		if opponent.val != 'p':
+			win_flag = False
+
+	# Print summary and update wins/losses
+	if draw_flag:
+		print('---> Draw!')
+
+	elif win_flag:
+		print(f'---> {player.name} wins!')
+		player.w += 1
+		opponent.l += 1
+
+	else:
+		print(f'---> {opponent.name} wins!')
+		player.l += 1
+		opponent.w += 1
+
+	print(f'{player.name} total wins: {player.w}/{game_count}, total lose: {player.l}/{game_count}')
+	print(f'{opponent.name} total wins: {opponent.w}/{game_count}, total lose: {opponent.l}/{game_count}')
+
+# Main function
+def main():
+	# RPS Dict for printing
+	rps_dict = {
+		'r' : 'rock',
+		'p' : 'paper',
+		's' : 'scissors'
+	}
+
+	# Title
+	print('Roshambo Game\n')
+
+	# Get name
+	player_name = input('Enter your name: ')
+	print()
+
+	# Hints
+	print(
+		'Hint 1: Bart\'s Roshambo is always rock.\n'
+		'Hint 2: Lisa\'s Roshambo is selected by random.\n'
+	)
+
+	# Select opponent
+	opponent_name = input('Would you like to play against Bart or Lisa? (b/B or l/L): ').strip().lower()
+	print()
+
+	# Create player/opponent objects
+	player = Player(player_name)
+	if opponent_name == 'b': 
+		opponent = Bart()
+	else: 
+		opponent = Lisa()
+
+	# Game count
+	game_count = 0
+
+	# Loop for playing game and continuing/quitting
+	cont_flag = True
+	while cont_flag:
+		# Get player value
+		pval = input('Rock, paper, or scissors? (r/p/s): ').strip().lower()
+		print()
+
+		# Check if rps value is valid and play, else try again
+		if pval in ['r', 'p', 's']:
+			# Since valid, increase game count
+			game_count += 1
+
+			# Set player value, opponent value, print values, and play
+			player.val = pval
+			opponent.generateRoshambo()
+			print(f'{player.name}: {rps_dict[player.val]}')
+			print(f'{opponent.name}: {rps_dict[opponent.val]}')
+			play(player, opponent, game_count)
+			print()
+
+			# Ask to play again/continue
+			cont = input('Play again? (y/n): ').strip().lower()
+			print()
+
+			# User wants to quit
+			if cont != 'y':
+				print('Thanks for playing!')
+				cont_flag = False
+
+		# Invalid rps choice, try again
+		else:
+			print('Invalid choice. Try again.\n')
+
+# If running file directly, run main function
+if __name__ == '__main__':
+	main()
